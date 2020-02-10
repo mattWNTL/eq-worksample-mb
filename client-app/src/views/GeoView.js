8c4b16be804e56133693bbbd62a4a92ab39f9311
Matthew Ball <matt@whitenomad.ca>
2020-02-09 23:03:52 -0500

    Zoom to user location

--- client-app/src/views/GeoView.js
+++ client-app/src/views/GeoView.js
@@ -5,15 +5,38 @@ import GeoMap from './components/Map';
 
 const GeoView = () => {
     const {loading, data} = useSelector(state => state.data) 
+    const [lat, setLat] = useState()
+    const [long, setLong] = useState()
     const dispatch = useDispatch()
 
     useEffect(() => {
+        if (!navigator.geolocation) {
+            //status.textContent = 'Geolocation is not supported by your browser';
+        } else {
+            //status.textContent = 'Locating…';
+            navigator.geolocation.getCurrentPosition(locationSuccess, locationError);
+        }
+
         dispatch(fetchPOI())
     }, [])
 
+    function locationSuccess(position){
+        const latitude  = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        setLat(latitude)
+        setLong(longitude)
+    }
+
+    function locationError(){
+        //err
+    }
+
+
+
     return(
         <div className="card mt-3 p-3" style={{height: 600, width: 1000, margin: 'auto'}}>
-            <GeoMap data={data}/>
+            <GeoMap data={data} lat={lat} long={long}/>
         </div>
     )
 }
--- client-app/src/views/components/Map.js
+++ client-app/src/views/components/Map.js
@@ -1,27 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import GoogleMapReact from 'google-map-react';
 
 const LocationComponent = ({ text }) => <div>{text}</div>;
 
-const GeoMap = ({data}) => {
+const GeoMap = ({data, lat, long}) => {
+  const [loading, setLoading] = useState(true)
+    useEffect(() => {
+      if(lat && long){
+        setLoading(false)
+      }
+    }, [lat, long])
     const mapOptions = {
         center: {
-            lat: 59.95,
-            lng: 30.33
+            lat: lat,
+            lng: long
         },
-        zoom: 1
+        zoom: 8
     }
 
     const returnLocationComponents = data.length ? data.map((x) => <LocationComponent lat={x.lat} lng={x.lon} text={x.name}/>) : []
 
-    return(
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyBzw9RZqRjpNacNglEUVHOT67Wttu-yxtM'}}
-          defaultCenter={mapOptions.center}
-          defaultZoom={mapOptions.zoom}
-        >
-          {returnLocationComponents}
-        </GoogleMapReact>
+    return (
+      <>
+        {loading ?
+          <div className="loader"></div>
+        :
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: process.env.GMAPKEY}}
+            defaultCenter={mapOptions.center}
+            defaultZoom={mapOptions.zoom}
+          >
+            {returnLocationComponents}
+          </GoogleMapReact>
+        }
+      </>
     )
 }
 
